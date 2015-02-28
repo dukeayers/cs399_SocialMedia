eba5f97a56fb38be3f2a05f7f2c3dfada2f6abe0
Duke Ayers <Dfa24@nau.edu>
2015-02-28 15:30:33 -0700

    Added a basic login page
    Made a basic login page with some basic validation to check if the
    username/password is correct.

--- social/social/models.py
+++ social/social/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
+from django import forms
+
 # Create your models here.
 
 class User(models.Model):
@@ -10,6 +12,9 @@ class User(models.Model):
     email_address = models.EmailField(max_length = 40)
 
 class UserLogin(ModelForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'required': 'required', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'type': 'password', 'required': 'required', 'placeholder': 'Password'}))
+
     class Meta:
         model = User
         fields = ['username', 'password']
--- social/views.py
+++ social/views.py
@@ -1,9 +1,21 @@
 from django.core import serializers
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-
+from social.models import UserLogin, User
 def home(request):
-    return render(request, 'index.html')
+    error = ""
+    if request.method == 'POST':
+        form = UserLogin(request.POST)
+        if(form.is_valid()):
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            if User.objects.filter(username = username, password = password).count() == 1:
+                return HttpResponse("Good")
+            else:
+                error = "Username/Password is incorrect."
+    else:
+        form = UserLogin()
+    return render(request, 'login.html', {'form': form, 'error': error})
 
 
 
