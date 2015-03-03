71d8ad802efff3a7c51723a331ed2ba910ebe240
Nancy <nm389@nau.edu>
2015-03-03 15:01:35 -0700

    'add user content' table, model, and form created
    please take a look over all this! I'm not sure I did everything right
    yet.

--- social/social/models.py
+++ social/social/models.py
@@ -4,3 +4,18 @@ from django.forms import ModelForm
 from django import forms
 
 # Create your models here.
+class Content(models.Model):
+    url = models.URLField()
+    description = models.TextField(max_length = 200)
+    username = models.CharField(max_length = 30)
+    datetime = models.DateTimeField()
+    image = models.URLField()
+
+class User_Content(ModelForm):
+    url = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'required': 'required', 'placeholder': 'Website URL'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'required': 'required', 'placeholder': 'Description'}))
+    class Meta:
+        model = Content
+        fields = ['url', 'description']
+        
+        
