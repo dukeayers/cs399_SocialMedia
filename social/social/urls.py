b9cba3d8419ab11012486a2b8e5e0242ee7b65ea
Nancy McCollough <nm389@nau.edu>
2015-04-02 16:04:49 -0700

    added settings page basics

--- social/social/urls.py
+++ social/social/urls.py
@@ -15,5 +15,6 @@ urlpatterns = patterns('',
 	url(r'^signup/', signup),
 	url(r'^about/', about),
     url(r'^profile/', profile),
+	url(r'^settings/', settings),
     url(r'^admin/', include(admin.site.urls)),
 )
